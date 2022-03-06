e4b3d335b10a0644f2799eef8596ba7e18fe80c1
xaviering <xaviering@outlook.com>
2022-03-06 15:19:31 +0800

    测试项目

---
+++ src/a.java
@@ -0,0 +1,16 @@
+public class a {
+    public static void main(String[] args) {
+
+    }
+
+    void fun(int n){
+        int s = 0;
+        for (int i=0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                for (int k = 0; k <= j; k++) {
+
+                }
+            }
+        }
+    }
+}
